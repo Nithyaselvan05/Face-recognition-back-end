7767f0a9a63bf295c24553488cd17747646a72fe
Nithyaselvan05 <nithyaselvan007@gmail.com>
2021-06-23 12:53:35 +0530

    Updating db connection

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const clarifai=require('clarifai')
 const db=knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl:true
   }
 });
